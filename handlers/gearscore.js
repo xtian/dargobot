aa6ba6fa62643afd3b56f3a5a1ba8aab0884b24b
Christian Wesselhoeft <hi@xtian.us>
2015-04-26 14:54:35 -0600

    Use dedent tagged strings.

--- handlers/gearscore.js
+++ handlers/gearscore.js
@@ -1,12 +1,15 @@
+import dedent from 'dedent'
+
 import Character from '../models/character'
 
 const handler = (text) => {
   const [_, name, realm, region] = text.split(' ')
 
   return Character.fetch(name, realm, region).then(char => {
-    const message =
-      `_Gearscore for ${char.name} of ${char.region}-${char.realm}:_` +
-      `\n*${char.gearscore.toString()}*`
+    const message = dedent`
+      _Gearscore for ${char.name} of ${char.region}-${char.realm}:_
+      *${char.gearscore.toString()}*
+      `
 
     return message
   })
--- handlers/ilvl.js
+++ handlers/ilvl.js
@@ -1,12 +1,15 @@
+import dedent from 'dedent'
+
 import Character from '../models/character'
 
 const handler = (text) => {
   const [_, name, realm, region] = text.split(' ')
 
   return Character.fetch(name, realm, region).then(char => {
-    const message =
-      `_Average iLvl for ${char.name} of ${char.region}-${char.realm}:_` +
-      `\n*${char.averageItemLevel.toString()}*`
+    const message = dedent`
+      _Average iLvl for ${char.name} of ${char.region}-${char.realm}:_
+      *${char.averageItemLevel.toString()}*
+      `
 
     return message
   })
