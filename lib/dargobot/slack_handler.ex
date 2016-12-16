11cbbd5a49e03be393965aa7fe0a22f0df539ab4
Christian Wesselhoeft <hi@xtian.us>
2016-12-16 16:32:26 -0700

    SlackHandler: Simplify by removing DI

--- lib/dargobot/slack_handler.ex
+++ lib/dargobot/slack_handler.ex
@@ -3,21 +3,17 @@ defmodule Dargobot.SlackHandler do
   Handles incoming messages and sends responses
   """
 
-  alias Dargobot.Router
-
   use Slack
 
-  def handle_event(message, slack, state, dispatch \\ &Router.dispatch/3)
-  def handle_event(message = %{type: "message"}, _, state, dispatch) do
-    :ok = dispatch.(message.text, message.channel, self())
+  def handle_event(message = %{type: "message"}, _, state) do
+    :ok = Dargobot.Router.dispatch(message.text, message.channel, self())
     {:ok, state}
   end
-  def handle_event(_, _, state, _), do: {:ok, state}
+  def handle_event(_, _, state), do: {:ok, state}
 
-  def handle_info(message, slack, state, send_message \\ &send_message/3)
-  def handle_info({:message, text, channel}, slack, state, send_message) do
-    :ok = send_message.(text, channel, slack)
+  def handle_info({:message, text, channel}, slack, state) do
+    :ok = send_message(text, channel, slack)
     {:ok, state}
   end
-  def handle_info(_, _, state, _), do: {:ok, state}
+  def handle_info(_, _, state), do: {:ok, state}
 end
